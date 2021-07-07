5abe004be44aaf48983d330f6ce4dd821f87754b
Bruna Marques <brmarques1108@gmail.com>
2021-07-07 21:49:24 +0100

    question room, auth and routes done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,41 @@
 import {useHistory} from 'react-router-dom';
+import { FormEvent, useState } from 'react';
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button'
-import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss'
 
 export function Home(){
     const history = useHistory();
     const {signInWithGoogle, user} = useAuth()
+    const [ roomCode, setRoomCode] = useState('')
 
     async function handleCreateRoom() {
         if(!user) {
             await signInWithGoogle()
         }
         history.push('/rooms/new')
+    }
+
+    async function handleJoinRoom( event: FormEvent) {
+        event.preventDefault();
+
+        if(roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+        if(!roomRef.exists()){
+            alert('Room does not exists.');
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`)
     }
 
     return(
@@ -36,10 +55,12 @@ export function Home(){
                         Create a room with your Google email
                     </button>
                     <div className="separator">or join an existing room</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input
                         type='text'
                         placeholder='Enter the room code'
+                        onChange={event => setRoomCode(event.target.value)}
+                        value={roomCode}
                         />
                         <Button type='submit'>Join the room</Button>
                     </form>
